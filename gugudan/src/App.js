5378312543c71789f2473a75d4ae836e618073af
hongdahee <heewhy3513@gmail.com>
2022-08-03 13:55:35 +0900

    feat: 구구단, 끝말잇기 필수 기능 구현

---
+++ gugudan/src/App.js
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+  <div>
+    <GuGuDan />  
+  </div>
+  );
+}
+
+const GuGuDan = () => {
+
+  const [first, setFirst] = useState(Math.ceil(Math.random()*9));
+  const [second, setSecond] = useState(Math.ceil(Math.random()*9));
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  
+  const right = () => {
+      setResult("🙆‍♀️ 딩동댕~");
+      setFirst(Math.ceil(Math.random()*9));
+      setSecond(Math.ceil(Math.random()*9));
+      setValue("");
+  }
+
+  const wrong = () => {
+    setResult("🙅‍♂️ 땡!");
+    setValue("");
+  }
+
+  return(
+    <div className='app'>
+      <h2>구구단 게임 💬</h2>
+      <div>{first} 곱하기 {second}은(는)?</div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        {Number(value) === (first*second) ? right() : wrong()}
+        }}>
+        <input type= "number" value={value} onChange = {(e) => {setValue(e.target.value);}} />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </div>
+)
+
+}
+
+
+export default App;
